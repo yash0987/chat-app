819be38b81c610c66ca4383f6de32077de36f2c9
yash0987 <84738197+yash0987@users.noreply.github.com>
2023-08-16 21:35:05 +0530

    Change location of redux store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import chatReducer from '../features/chat-slice/chatSlice';
 import authReducer from '../features/auth-slice/authSlice';
+import modalReducer from '../features/modal-slice/modalSlice';
+import selectMessageReducer from '../features/select-message-slice/selectMessageSlice';
 
 export default configureStore({
     reducer: {
         chat: chatReducer,
         auth: authReducer,
+        modal: modalReducer,
+        selectmessage: selectMessageReducer
     }
 })
