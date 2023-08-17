09700379c7de6bcbf0a36e65932eb0ab794cafca
yash0987 <84738197+yash0987@users.noreply.github.com>
2023-08-17 18:10:41 +0530

    Rename modal component

--- client/src/Components/modal/DeleteForMeModal.js
+++ client/src/Components/modal/DeleteForMeModal.js
@@ -4,18 +4,20 @@ import { updateChat } from '../../features/chat-slice/chatSlice';
 import { showDeleteModal } from './../../features/modal-slice/modalSlice';
 import { unselectAllMessages } from '../../features/select-message-slice/selectMessageSlice';
 
-export default function DeleteMessage(props) {
-  const selectedMessagesList = useSelector(state => state.selectmessage.value);
+export default function DeleteForMeModal(props) {
   const chat = useSelector(state => state.chat.value);
+  const deleteModalState = useSelector(state => state.modal.value[0]);
+  const selectedMessagesList = useSelector(state => state.selectmessage.value);
   const dispatch = useDispatch();
 
-  async function deleteMessages() {
+  async function deleteForMe() {
     let remainingMessages = chat;
     selectedMessagesList.forEach((elementToRemove) => {
       remainingMessages = remainingMessages.filter((element) => element.messageID !== elementToRemove);
     })
     dispatch(updateChat(remainingMessages))
     dispatch(unselectAllMessages());
+    dispatch(showDeleteModal());
     props.setDeleteToggle(false);
 
     const deleteMessageRequestURI = `http://localhost:5000/delete/messages?selectedMessages=${JSON.stringify(selectedMessagesList)}&ID=${props.room}`;
@@ -33,13 +35,8 @@ export default function DeleteMessage(props) {
     console.log(data);
   }
 
-  function deleteForMe() {
-    deleteMessages();
-    props.setDeleteToggle(false);
-    dispatch(showDeleteModal());
-  }
-  
   return (
+    deleteModalState && selectedMessagesList.length > 0 ? 
     <section className='flex justify-center py-80 w-screen h-screen fixed top-0 left-0 bg-black bg-opacity-20'>
       <div className='p-5 w-1/5 rounded-sm bg-violet-50'>
         <p>{ selectedMessagesList.length < 2 ? `Delete message?` : `Delete ${selectedMessagesList.length} messages?` }</p>
@@ -48,6 +45,6 @@ export default function DeleteMessage(props) {
           <button onClick={ () => dispatch(showDeleteModal()) }>Cancel</button>
         </div>
       </div>
-    </section>
+    </section> : null
   )
 }
