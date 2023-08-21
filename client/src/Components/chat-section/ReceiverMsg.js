e99e86b06e7f08f6bae59e972769240a76fcd5a8
yash0987 <84738197+yash0987@users.noreply.github.com>
2023-08-21 18:46:17 +0530

    Fix bug of selected messages array not get empty

--- client/src/Components/chat-section/ReceiverMsg.js
+++ client/src/Components/chat-section/ReceiverMsg.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectMessage, unselectMessage } from '../../features/select-message-slice/selectMessageSlice';
+import { unselectAllMessages } from '../../features/select-message-slice/selectMessageSlice';
 
 export default function ReceiverMsg(props) {
   const [highlightSelectMessage, setHighlightSelectMessage] = useState(0);
+  const toggleFeaturesState = useSelector(state => state.toggle.value.toggleFeatures);
   const dispatch = useDispatch();
 
   function selectAndUnselectMessage() {
@@ -21,13 +23,16 @@ export default function ReceiverMsg(props) {
   }
 
   useEffect(() => {
-    if (highlightSelectMessage && !props.deleteToggle) setHighlightSelectMessage(0);
+    if (highlightSelectMessage && !toggleFeaturesState) {
+      setHighlightSelectMessage(0);
+      dispatch(unselectAllMessages());
+    }
     // eslint-disable-next-line
-  }, [props.deleteToggle]);
+  }, [toggleFeaturesState]);
 
   return (
-    <section onClick={ () => props.deleteToggle ? selectAndUnselectMessage() : null } className = {`${ highlightSelectMessage && props.deleteToggle ? 'bg-violet-500 bg-opacity-20' : 'bg-none' } select-none rounded-md flex justify-start my-1 p-1`} data-person='receiver'>
-      { props.deleteToggle ? <div className='m-2'>&#9552;</div> : null }
+    <section onClick={ () => toggleFeaturesState ? selectAndUnselectMessage() : null } className = {`${ highlightSelectMessage && toggleFeaturesState ? 'bg-violet-500 bg-opacity-20' : 'bg-none' } select-none rounded-md flex justify-start my-1 p-1`} data-person='receiver'>
+      { toggleFeaturesState ? <div className='m-2'>&#9552;</div> : null }
       <p className = 'whitespace-pre-wrap break-words max-w-xs px-2 py-1 rounded-md text-white bg-violet-500'>
         { props.element.collectedText }
         <span className = 'flex justify-end text-[10px] text-white'> { props.element.star ? <span>&#9733;  </span> : null } { props.element.currentMsgTime } </span>
--- client/src/Components/chat-section/SenderMsg.js
+++ client/src/Components/chat-section/SenderMsg.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { selectMessage, unselectMessage } from '../../features/select-message-slice/selectMessageSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectMessage, unselectAllMessages, unselectMessage } from '../../features/select-message-slice/selectMessageSlice';
 
 export default function SenderMsg(props) {
   const [highlightSelectMessage, setHighlightSelectMessage] = useState(0);
+  const toggleFeaturesState = useSelector(state => state.toggle.value.toggleFeatures);
   const dispatch = useDispatch();
 
   function selectAndUnselectMessage() {
@@ -21,17 +22,20 @@ export default function SenderMsg(props) {
   }
 
   useEffect(() => {
-    if (highlightSelectMessage && !props.deleteToggle) setHighlightSelectMessage(0);
+    if (highlightSelectMessage && !toggleFeaturesState) {
+      setHighlightSelectMessage(0);
+      dispatch(unselectAllMessages());
+    }
     // eslint-disable-next-line
-  }, [props.deleteToggle]);
+  }, [toggleFeaturesState]);
   
   return (
-    <section onClick={ () => props.deleteToggle ? selectAndUnselectMessage() : null } className = {`${ highlightSelectMessage && props.deleteToggle ? 'bg-violet-500 bg-opacity-20' : 'bg-none' } select-none rounded-md flex justify-end my-1 p-1`} data-person='sender'>
+    <section onClick={ () => toggleFeaturesState ? selectAndUnselectMessage() : null } className = {`${ highlightSelectMessage && toggleFeaturesState ? 'bg-violet-500 bg-opacity-20' : 'bg-none' } select-none rounded-md flex justify-end my-1 p-1`} data-person='sender'>
       <p className = 'whitespace-pre-wrap break-words px-2 py-1 max-w-xs rounded-md bg-gray-200 text-gray-600'>
         { props.element.collectedText }
         <span className = 'flex justify-end text-[10px]'> { props.element.star ? <span>&#9733;  </span> : null } { props.element.currentMsgTime } </span>
       </p>
-      { props.deleteToggle ? <div className='m-2'>&#9552;</div> : null }
+      { toggleFeaturesState ? <div className='m-2'>&#9552;</div> : null }
     </section>
   )
 }
