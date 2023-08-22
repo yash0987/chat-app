814327a32ec433ad60342823887c6efc8b99d393
yash0987 <84738197+yash0987@users.noreply.github.com>
2023-08-22 14:04:42 +0530

    Change DOM with useState hook in friend request section

--- client/src/Components/friend-section/friend-requests/FriendRequests.js
+++ client/src/Components/friend-section/friend-requests/FriendRequests.js
@@ -5,29 +5,13 @@ import SendSection from './SendSection';
 export default function FriendRequests() {
   const [requestBlock, setRequestBlock] = useState('receive');
 
-  function setReceiveRequestList(event) {
-    event.target.className = ' border-b-2 border-violet-500 mx-1 py-2 w-44 text-violet-600';
-    event.target.nextElementSibling.className = 'mx-1 py-2 w-44 text-violet-400';
-    setRequestBlock('receive');
-  }
-
-  function setSendRequestList(event) {
-    event.target.previousElementSibling.className = 'mx-1 py-2 w-44 text-violet-400';
-    event.target.className = 'border-b-2 border-violet-500 mx-1 py-2 w-44 text-violet-600';
-    setRequestBlock('send');
-  }
-
   return (
     <section>
       <div className='flex justify-center my-4'>
-        <button onClick={setReceiveRequestList} id="receiveBtn" className='mx-1 py-2 w-44 text-violet-600 border-b-2 border-violet-500'>Receive</button>
-        <button onClick={setSendRequestList} id="sendBtn" className='mx-1 py-2 w-44 text-violet-400'>Send</button>
+        <button onClick={() => setRequestBlock('receive')} id="receiveBtn" className={`${requestBlock === 'receive' ? 'border-b-2 border-violet-500 text-violet-600': 'text-violet-400' } mx-1 py-2 w-44`}>Receive</button>
+        <button onClick={() => setRequestBlock('send')} id="sendBtn" className={`${requestBlock === 'send' ? 'border-b-2 border-violet-500 text-violet-600': 'text-violet-400' } mx-1 py-2 w-44`}>Send</button>
       </div>
-      <div>
-        {
-          requestBlock === 'receive' ? <ReceiveSection /> : <SendSection />
-        }
-      </div>
+      <div>{ requestBlock === 'receive' ? <ReceiveSection /> : <SendSection /> }</div>
     </section>
   )
 }
