c6782e4c617f9e09dacc0b8df6fd09abc837c180
yash0987 <84738197+yash0987@users.noreply.github.com>
2023-09-10 11:30:32 +0530

    Activate toggle features on hold received message

--- client/src/Components/chat-section/ReceiverMsg.js
+++ client/src/Components/chat-section/ReceiverMsg.js
@@ -1,15 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectMessage, unselectMessage } from '../../features/select-message-slice/selectMessageSlice';
 import { unselectAllMessages } from '../../features/select-message-slice/selectMessageSlice';
+import { toggleFeatures } from '../../features/toggle-slice/toggleSlice';
 
 export default function ReceiverMsg(props) {
   const [highlightSelectMessage, setHighlightSelectMessage] = useState(0);
+  const holdEventTimer = useRef(null);
   const toggleFeaturesState = useSelector(state => state.toggle.value.toggleFeatures);
   const dispatch = useDispatch();
 
   function selectAndUnselectMessage() {
-    setHighlightSelectMessage(highlightSelectMessage ^ 1)
+    console.log("I am clicking")
+    setHighlightSelectMessage(highlightSelectMessage ^ 1);
     if (!highlightSelectMessage) {
       console.log("select message")
       dispatch(selectMessage(props.element.messageID));
@@ -17,7 +20,7 @@ export default function ReceiverMsg(props) {
     }
     else if (highlightSelectMessage) {
       console.log("unselect message")
-      dispatch(unselectMessage(props.element.messageID))
+      dispatch(unselectMessage(props.element.messageID));
       if (!props.element.star) props.setStar(props.star - 1);
     }
   }
@@ -30,8 +33,26 @@ export default function ReceiverMsg(props) {
     // eslint-disable-next-line
   }, [toggleFeaturesState]);
 
+  function triggerHoldEvent() {
+    if (toggleFeaturesState) {
+      selectAndUnselectMessage();
+      return ;
+    }
+    holdEventTimer.current = setTimeout(() => {
+      console.log("I am holding for selecting messages");
+      selectAndUnselectMessage();
+      dispatch(toggleFeatures());
+    }, 1000);
+  }
+
+  function stopHoldEvent() {
+    if (toggleFeaturesState) return ;
+    clearTimeout(holdEventTimer.current);
+    holdEventTimer.current = null;
+  }
+
   return (
-    <section onClick={ () => toggleFeaturesState ? selectAndUnselectMessage() : null } className = {`${ highlightSelectMessage && toggleFeaturesState ? 'bg-violet-500 bg-opacity-20' : 'bg-none' } select-none rounded-md flex justify-start my-1 p-1`} data-person='receiver'>
+    <section onMouseDown={ () => triggerHoldEvent() } onMouseUp={ () => stopHoldEvent() } onMouseLeave={ () => stopHoldEvent() } className = {`${ highlightSelectMessage && toggleFeaturesState ? 'bg-violet-500 bg-opacity-20' : 'bg-none' } select-none rounded-md flex justify-start my-1 p-1`} data-person='receiver'>
       { toggleFeaturesState ? <div className='m-2'>&#9552;</div> : null }
       <p className = 'whitespace-pre-wrap break-words max-w-lg px-2 py-1 rounded-md text-white bg-violet-500'>
         { props.element.collectedText }
