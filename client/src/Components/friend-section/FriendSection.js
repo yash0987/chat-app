c38b559cffb42f5dac153d3f2735b02177d5683c
yash0987 <84738197+yash0987@users.noreply.github.com>
2024-01-01 21:50:54 +0530

    Use hooks and remove DOM for update css

--- client/src/Components/friend-section/FriendSection.js
+++ client/src/Components/friend-section/FriendSection.js
@@ -1,46 +1,65 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import FriendRequests from './friend-requests/FriendRequests';
 import GroupSection from './groups/GroupSection';
 import AllFriendsSection from './all-friends/AllFriendsSection';
-import { useSelector } from 'react-redux';
+import personIcon from './../../img/personw.png';
+import groupIcon from './../../img/groupw.png';
+import inboxIcon from './../../img/inbox.png';
 
 export default function FriendList(props) {  
-  const [block, setBlock] = useState(1);
   const theme = useSelector(state => state.theme.value);
+  const buttonStyle = {
+    activeBtn: `${theme.bg400} rounded-3xl w-12 transition-all duration-200`,
+    inactiveBtn: `${theme.bg400} rounded-full hover:rounded-3xl w-12 transition-all duration-200`
+  }
 
-  function setFriendsList(event) {
-    event.target.className = `${theme.bg300} px-5 py-2 rounded-lg`;
-    event.target.nextElementSibling.className = `px-5 py-2 rounded-lg ${theme.hoverBg200}`;
-    event.target.parentElement.lastElementChild.className = `px-5 py-2 rounded-lg ${theme.hoverBg200}`;
+  const [block, setBlock] = useState(1);
+  const [firstBtnStyle, setFirstBtnStyle] = useState(buttonStyle.activeBtn);
+  const [secondBtnStyle, setSeocndBtnStyle] = useState(buttonStyle.inactiveBtn);
+  const [thirdBtnStyle, setThirdBtnStyle] = useState(buttonStyle.inactiveBtn);
+
+  function setFriendsList() {
+    setFirstBtnStyle(buttonStyle.activeBtn);
+    setSeocndBtnStyle(buttonStyle.inactiveBtn);
+    setThirdBtnStyle(buttonStyle.inactiveBtn);
     setBlock(1);
   }
   
-  function setGroupList(event) {
-    event.target.previousElementSibling.className = `px-5 py-2 rounded-lg ${theme.hoverBg200}`;
-    event.target.className = `${theme.bg300} px-5 py-2 rounded-lg`;
-    event.target.nextElementSibling.className = `px-5 py-2 rounded-lg ${theme.hoverBg200}`;
+  function setGroupList() {
+    setFirstBtnStyle(buttonStyle.inactiveBtn);
+    setSeocndBtnStyle(buttonStyle.activeBtn);
+    setThirdBtnStyle(buttonStyle.inactiveBtn);
     setBlock(2);
   }
 
-  function setFriendRequests(event) {
-    event.target.parentElement.firstElementChild.className = `px-5 py-2 rounded-lg ${theme.hoverBg200}`;
-    event.target.previousElementSibling.className = `px-5 py-2 rounded-lg ${theme.hoverBg200}`;
-    event.target.className = `${theme.bg300} px-5 py-2 rounded-lg`;
+  function setFriendRequests() {
+    setFirstBtnStyle(buttonStyle.inactiveBtn);
+    setSeocndBtnStyle(buttonStyle.inactiveBtn);
+    setThirdBtnStyle(buttonStyle.activeBtn);
     setBlock(3);
   }
  
   return (
-    <section className={`h-[91vh] border-l-[1px] ${theme.borderL700} font-semibold ${theme.bg50}`}>
-      <input type="search" name="" id="" placeholder='Search friends' className={`my-4 mx-9 px-5 py-1 w-[85%] rounded-sm ${theme.bg50} border-[1px] border-b-[3px] ${theme.border500} font-normal focus:outline-none ${theme.placeholderText400}`} />
-      <div className='mb-4 flex justify-evenly'>
-        <button onClick={setFriendsList} id="allFriends" className={`${theme.bg300} px-5 py-2 rounded-lg`}>All friends</button>
-        <button onClick={setGroupList} id="groups" className={`px-5 py-2 rounded-lg ${theme.hoverBg200}`}>Groups</button>
-        <button onClick={setFriendRequests} id="friendRequests" className={`px-5 py-2 rounded-lg ${theme.hoverBg200}`}>Friend Requests</button>
-      </div>
-      <div>{
+    <section className='grid grid-flow-col'>
+      <div className={`h-[91vh] border-l-[1px] ${theme.borderL700} font-semibold ${theme.bg50}`}>
+        <div className='min-w-[27rem]'>{
           block === 1 ? (<AllFriendsSection setOldChatPerson={props.setOldChatPerson} secondPerson={props.secondPerson} setSecondPerson={props.setSecondPerson} setToggle={props.setToggle} />) :
-          block === 2 ? (<GroupSection setToggle={props.setToggle} />) : (<FriendRequests />)
-      }</div>
+          block === 2 ? (<GroupSection setToggle={props.setToggle} setOldChatPerson={props.setOldChatPerson} secondPerson={props.secondPerson} setSecondPerson={props.setSecondPerson} />) : 
+          (<FriendRequests />)
+        }</div>
+      </div>
+      <div className='grid grid-flow-row grid-rows-10 m-2'>
+        <button onClick={() => setFriendsList()} id="allFriends">
+          <img src={personIcon} alt="" className={firstBtnStyle} />
+        </button>
+        <button onClick={() => setGroupList()} id="groups">
+          <img src={groupIcon} alt="" className={secondBtnStyle} />
+        </button>
+        <button onClick={() => setFriendRequests()} id="friendRequests">
+          <img src={inboxIcon} alt="" className={thirdBtnStyle} />
+        </button>
+      </div>
     </section>
   )
 }
