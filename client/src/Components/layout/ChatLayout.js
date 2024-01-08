405a674e5d720bd08644ff987efe281861594845
yash0987 <84738197+yash0987@users.noreply.github.com>
2024-01-08 18:51:22 +0530

    Create second layout to show chats

---
+++ client/src/Components/layout/ChatLayout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet, useOutletContext } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import AllFriendsSection from '../friend-section/all-friends/AllFriendsSection';
+
+export default function ChatLayout() {
+  const theme = useSelector(state => state.theme.value);
+  const { toggle, setToggle, secondPerson, setSecondPerson, oldChatPerson, setOldChatPerson } = useOutletContext();
+
+  return (
+    <div className={`grid grid-flow-col ${theme.bg100}`}>
+      <Outlet context={{ toggle, setToggle, secondPerson, setSecondPerson, oldChatPerson, setOldChatPerson }} />
+      <AllFriendsSection setOldChatPerson={setOldChatPerson} setSecondPerson={setSecondPerson} secondPerson={secondPerson} setToggle={setToggle} />
+    </div>
+  )
+}
