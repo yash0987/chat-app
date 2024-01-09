1358261e80efa8403eac8c0cc3b321e0abb1b66d
yash0987 <84738197+yash0987@users.noreply.github.com>
2024-01-09 08:58:04 +0530

    Modify App.js for routing using react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,32 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './app/store';
 import LoginPage from './components/LoginPage';
-import HomePage from './components/HomePage';
-import './App.css';
+import DefaultPage from './components/DefaultPage';
+import ChatSection from './components/chat-section/ChatSection';
+import HomeLayout from './components/layout/HomeLayout';
+import ChatLayout from './components/layout/ChatLayout';
+import GroupLayout from './components/layout/GroupLayout';
+import RequestLayout from './components/layout/RequestLayout';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<LoginPage />} />
-      <Route path='/Home' element={<Provider store={store}><HomePage /></Provider>} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route element={<Provider store={store}><HomeLayout /></Provider>}> {/* change position of layout */}
+          <Route path='/chats/@me' element={<ChatLayout />}>
+            <Route path='/chats/@me' element={<DefaultPage />} />
+            <Route path='/chats/@me/:id' element={<ChatSection />} />
+          </Route>
+          <Route path='/groups' element={<GroupLayout />}>
+            <Route path='/groups' element={<DefaultPage />} />
+            <Route path='/groups/:groupid' element={<ChatSection />} />
+          </Route>
+          <Route path='/requests' element={<RequestLayout />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
