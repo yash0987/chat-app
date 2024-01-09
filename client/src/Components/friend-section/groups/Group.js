464ba3d0c0a92a7ebb8efcf47ba93dc6ed0a1f0f
yash0987 <84738197+yash0987@users.noreply.github.com>
2024-01-09 09:40:30 +0530

    Modify Group.js using Link to route groups chat

--- client/src/Components/friend-section/groups/Group.js
+++ client/src/Components/friend-section/groups/Group.js
@@ -1,25 +1,37 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { emptyChat } from '../../../features/chat-slice/chatSlice';
+import { unselectAllMessages } from '../../../features/select-message-slice/selectMessageSlice';
+import { replyMessageToggle, starMessagesToggle } from '../../../features/toggle-slice/toggleSlice';
 
 export default function Group(props) {
   const theme = useSelector(state => state.theme.value);
+  const dispatch = useDispatch();
 
   function openChat() {
-    
+    console.log(props.groupInfo);
+    const ID = props.groupInfo.groupID;
+    const fullName = props.groupInfo.groupName;
+    const photoURL = props.groupInfo.groupPhotoURL;
+    props.setOldChatPerson(props.secondPerson);
+    props.setSecondPerson( { ID, fullName, photoURL } );
+    props.setToggle('showChatSection');
+    dispatch(unselectAllMessages());
+    dispatch(starMessagesToggle(false));
+    dispatch(replyMessageToggle(false));
+    dispatch(emptyChat());
   }
 
   return (
-    <section className={`flex justify-between mx-1 px-6 py-2 rounded-lg font-semibold ${theme.hoverBg100}`}>
-        <div className='flex'>
-            <img src={ props.GroupInfo.groupPhotoURL } alt="" className='w-10 rounded-full' />
-            <div className='mx-4'>
-              <p>{ props.GroupInfo.groupName }</p>
-              <p className='text-[10px] text-gray-400'>{ props.GroupInfo.groupID }</p>
-            </div>
+    <Link to={`/groups/${props.groupInfo.groupID}`} onClick={() => openChat()} className={`flex justify-between mx-2 px-6 py-2 rounded-lg font-semibold ${theme.hoverBg100}`}>
+      <div className='flex'>
+        <img src={ props.groupInfo.groupPhotoURL } alt="" className='w-10 rounded-full' />
+        <div className='mx-4'>
+          <p>{ props.groupInfo.groupName }</p>
+          <p className='text-[10px] text-gray-400'>{ props.groupInfo.groupID }</p>
         </div>
-        <div>
-            <button onClick={openChat} className='my-2 px-3 py-1 text-xs rounded border-[1px] border-gray-400'>Message</button>
-        </div>
-    </section>
+      </div>
+    </Link>
   )
 }
