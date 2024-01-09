c0d2dfd56b631759028d996ac568b5293d236577
yash0987 <84738197+yash0987@users.noreply.github.com>
2024-01-09 09:36:01 +0530

    Combine parent component JSX with GroupSection.js

--- client/src/Components/friend-section/groups/GroupSection.js
+++ client/src/Components/friend-section/groups/GroupSection.js
@@ -34,19 +34,23 @@ export default function GroupSection(props) {
   }
 
   return (
-    <section>
-      <input type="search" name="" id="" placeholder='Search Groups' className={`my-4 mx-9 px-5 py-1 w-[85%] rounded-sm ${theme.bg50} border-[1px] border-b-[3px] ${theme.border500} font-normal focus:outline-none ${theme.placeholderText400}`} />
-      <button onClick={createGroupComp} className={`p-1 w-full flex justify-center ${theme.text400} ${theme.hoverBg200}`}>
-        <p className='p-2'>Create new group</p>
-        <span className='text-3xl'>&#43;</span>
-      </button>
+    <section className='grid grid-flow-col'>
+      <div className={`h-[91vh] border-l-[1px] ${theme.borderL700} font-semibold ${theme.bg50}`}>
+        <div className='min-w-[27rem]'>
+          <input type="search" name="" id="" placeholder='Search Groups' className={`my-4 mx-9 px-5 py-1 w-[85%] rounded-sm ${theme.bg50} border-[1px] border-b-[3px] ${theme.border500} font-normal focus:outline-none ${theme.placeholderText400}`} />
+          <button onClick={createGroupComp} className={`p-1 w-full flex justify-center ${theme.text400} ${theme.hoverBg200}`}>
+            <p className='p-2'>Create new group</p>
+            <span className='text-3xl'>&#43;</span>
+          </button>
 
-      {
-        groups ? groups.map((element) => {
-          keyValue++;
-          return <Groups key={keyValue} groupInfo={element} setToggle={props.setToggle} setOldChatPerson={props.setOldChatPerson} secondPerson={props.secondPerson} setSecondPerson={props.setSecondPerson} />
-        }) : (getGroups())
-      }
+          {
+            groups ? groups.map((element) => {
+              keyValue++;
+              return <Groups key={keyValue} groupInfo={element} setToggle={props.setToggle} setOldChatPerson={props.setOldChatPerson} secondPerson={props.secondPerson} setSecondPerson={props.setSecondPerson} />
+            }) : (getGroups())
+          }
+        </div>
+      </div>
     </section>
   )
 }
