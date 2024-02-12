16061a94f236c0d1d13488446d7744b1c82e7e9f
yash0987 <84738197+yash0987@users.noreply.github.com>
2024-02-12 17:12:46 +0530

    Add preview of wallpaper for shared interface

--- client/src/Components/layout/ThemeLayout.js
+++ client/src/Components/layout/ThemeLayout.js
@@ -5,11 +5,20 @@ import ThemesPreview from '../sidebar/ThemesPreview';
 
 export default function ThemeLayout() {
   const theme = useSelector(state => state.theme.value);
+  const wallpaper = useSelector(state => state.wallpaper.value);
 
   return (
     <div className={`grid grid-flow-col ${theme.bg100}`}>
       <ThemesPreview />
-      <Outlet />
+      <section className='py-4 w-[27rem] h-full bg-white'>
+        <div className='grid grid-flow-row place-content-center text-gray-700 text-sm'>
+        Appearance
+          <div style={ wallpaper ? {backgroundImage: `url('${wallpaper}')`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat"} : null} className={`grid place-content-center my-1 w-[343px] h-[195.37px] rounded text-sm border-[1px] ${theme.border500} ${theme.text500} ${theme.bg50}`}>
+            { wallpaper ? null : "Default Wallpaper" }
+          </div>
+        </div>
+        <Outlet />
+      </section>
     </div>
   )
 }
