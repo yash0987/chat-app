e4059d4a24cd656430577269c71beeb58eafddac
yash0987 <84738197+yash0987@users.noreply.github.com>
2024-02-29 20:30:52 +0530

    Update profile section for new layout

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,85 +1,69 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import Chatbox from './Chatbox';
+import { fetchRequest } from 'utils/fetchRequest';
 
-export default function Profile(props) {
-  const [friendInfo, setFriendInfo] = useState(null);
-
-  function getFriendInfo() {
-    console.log(props.secondPerson);
-    async function getData() {
-      const friendDataRequestURI = `http://localhost:5000/friend/data/${props.secondPerson.ID}`;
-      const response = await fetch(friendDataRequestURI, {
-        method: 'GET',
-        credentials: 'include',
-        headers: {
-          Accept: "application/json",
-          "Content-type": "application/json",
-          "Access-Control-Allow-Credentials": true
-        }
-      });
-
-      const data = await response.json();
-      return data;
-    }
-
-    getData().then((data) => setFriendInfo(data));
-  }
-
-  async function unfriend() {
-    const unfriendRequestURI = `http://localhost:5000/unfriend/${friendInfo.googleID}`;
-    const response = await fetch(unfriendRequestURI, {
-      method: 'PUT',
-      credentials: 'include',
-      headers: {
-        Accept: "application/json",
-        "Content-type": "application/json",
-        "Access-Control-Allow-Credentials": true
-      }
-    });
-
-    const data = response.json();
-    console.log(data);
-  }
+export default function Profile() {
+  const [chatList, setChatList] = useState([]);
+  const theme = useSelector(state => state.theme.value);
+  const newChat = useSelector(state => state.chatinfo.value.newChat);
+  const [openList, setOpenList] = useState(false);
+  
+  useEffect(() => {
+    setChatList([]);
+    setOpenList(false);
+  }, [newChat.ID]);
 
   async function commonGroups() {
-    async function getData() {
-      const commonGroupsListRequestURI = `http://localhost:5000/friend/groups/${friendInfo.googleID}`;
-      const response = await fetch(commonGroupsListRequestURI, {
-        method: 'GET',
-        credentials: 'include',
-        headers: {
-          Accept: "application/json",
-          "Content-type": "application/json",
-          "Access-Control-Allow-Credentials": true
-        }
-      });
-
-      const data = await response.json();
-      return data;
+    if (openList) {
+      setChatList([]);
+      setOpenList(false);
+      return ;
     }
-
-    getData().then((data) => console.log(data));
+    const commonGroupsListRequestURI = `http://localhost:5000/common/groups/${newChat.ID}`;
+    const data = await fetchRequest({ url: commonGroupsListRequestURI, method: 'GET' });
+    setChatList(data);
+    setOpenList(true);
   }
 
+
   return (
-    <section className='col-span-2 p-5 rounded bg-violet-50'>
-      {
-        friendInfo ?
-        (<div>
-          <div className='m-5 flex justify-center'>
-            <img src={props.secondPerson.photoURL} alt="" id='selectedProfileImage' className='w-36 h-36 rounded-full object-cover' />
-          </div>
+    <section>
+      <div className={`h-[9rem] p-4 ${theme.bg300}`}>
+        <img src={newChat.photoURL} alt="" className='relative -bottom-16 size-24 rounded-full object-cover' />
+      </div>
+      
+      <div className={`m-4 mt-14 p-2 text-xs rounded-lg ${theme.bg50}`}>
+        <p className='text-lg font-bold'>{ newChat.fullName }</p>
+        <p className='font-semibold'>{ newChat.ID }</p>
+        <hr className={`my-4 ${theme.border300}`} />
+        <p className='font-semibold'>{ newChat.isGroup ? 'DESCRIPTION' : 'ABOUT ME'}</p>
+        <p>This group is for fun and code.</p>
+        <hr className={`my-4 ${theme.border300}`} />
+        <p className='font-semibold'>CHATME MEMBER SINCE</p>
+        <p>date</p>
+      </div>
 
-          <div className='my-5'>
-            <p className='flex justify-center text-2xl font-semibold'>{ friendInfo.firstName + " " + friendInfo.familyName }</p>
-            <p className='flex justify-center text-sm'>{ friendInfo.googleID }</p>
-          </div>
+      <div className={`m-4 p-2 text-sm font-semibold rounded-lg ${theme.bg50}`}>
+        {
+          newChat.isGroup ?
+          <button>Group Members</button> :
+          <button onClick={() => commonGroups()}>Common Groups</button>
+        }
 
-          <div className='flex justify-center'>
-            <button onClick={unfriend} className='px-4 py-2 rounded text-red-500 font-semibold hover:bg-red-100'>Unfriend</button>
-            <button onClick={commonGroups} className='px-4 py-2 rounded text-violet-500 font-semibold hover:bg-violet-100'>Block</button>
-          </div>
-        </div>) : (getFriendInfo())
-      }
+        {
+          openList ?
+          <div className='m-2 max-h-56 overflow-y-scroll'>
+            {
+              chatList.map((element) => {
+                const route = newChat.isGroup ? `/groups/${element.id}` : `/chats/@me/${element.id}`;
+                const chatinfo = { ...element, isGroup: newChat.isGroup };
+                return <Chatbox key={element.id} chat={chatinfo} route={route} />
+              })
+            }
+          </div> : null
+        }
+      </div>
     </section>
   )
 }
