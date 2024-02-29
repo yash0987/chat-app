9d48b81169d235cc47919414993181eaa99b4040
yash0987 <84738197+yash0987@users.noreply.github.com>
2024-02-29 21:10:03 +0530

    Hide profile toggle on closed chat

--- client/src/views/navbar/Navbar.js
+++ client/src/views/navbar/Navbar.js
@@ -1,5 +1,6 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import { showProfileToggle } from 'features/toggle-slice/toggleSlice';
 import Searchbar from 'views/navbar/components/Searchbar';
 import searchIcon from 'assets/searchBlack.png';
@@ -13,6 +14,14 @@ export default function Navbar(props) {
   const showProfile = useSelector(state => state.toggle.value.showProfile);
   const dispatch = useDispatch();
   const searchbar = useRef();
+  const params = useParams();
+  
+  useEffect(() => {
+    console.log(Object.keys(params).length);
+    console.log("this is params length")
+    if (!Object.keys(params).length) dispatch(showProfileToggle(false));
+    // eslint-disable-next-line
+  }, [params])
 
   window.onclick = (event) => {
     if (event.target.firstElementChild) {
@@ -28,7 +37,7 @@ export default function Navbar(props) {
       <div ref={searchbar} className={`grid grid-flow-col justify-between px-4 py-2 border-b-[1px] ${theme.borderB700} ${theme.bg200}`}>
         <p className={`px-4 text-2xl ${theme.text400}`}>ChatMe</p>
         <div className='grid grid-flow-col gap-2 place-items-center'>
-          <img onClick={() => dispatch(showProfileToggle(showProfile ^ 1))} src={profileIcon} alt="" className={`size-8 rounded-full ${theme.hoverBg300}`} />
+          { Object.keys(params).length ? <img onClick={() => dispatch(showProfileToggle(showProfile ^ 1))} src={profileIcon} alt="" className={`size-8 rounded-full ${theme.hoverBg300}`} /> : null }
           <img onClick={() => setShowSearchBar(true)} src={searchIcon} alt="" className={`size-8 rounded-full ${theme.hoverBg300}`} />
           <img onClick={() => props.setSettingToggle(true)} src={settingIcon} alt="" className={`size-8 rounded-full ${theme.hoverBg300}`} />
           <img src={user.photoURL} alt="" className='size-8 rounded-full' />
