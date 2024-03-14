53d172db3c5cf28aac9e804e76ebc6d27ca12ac8
yash0987 <84738197+yash0987@users.noreply.github.com>
2024-03-14 23:29:41 +0530

    Generalize profile edit for user and groups

--- client/src/views/sidebar/components/Profile.js
+++ client/src/views/sidebar/components/Profile.js
@@ -2,13 +2,16 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setUser } from 'features/auth-slice/authSlice';
 import { dateFromEpoch } from 'utils/dateFromEpoch';
+import SelectionBox from './SelectionBox';
 
-export default function Profile(props) {
+export default function Profile() {
   const theme = useSelector(state => state.theme.value);
   const user = useSelector(state => state.auth.value.user);
   const [photo, setPhoto] = useState({});
   const [profileEditStatus, setProfileEditStatus] = useState(false);
-  const [profileDetails, setProfileDetails] = useState({ name: user.name, aboutMe: user.aboutMe, photoURL: user.photoURL });
+  const [profileDetails, setProfileDetails] = useState({ id: user.googleID, name: user.name, aboutMe: user.aboutMe, photoURL: user.photoURL });
+  const [profileSwitch, setProfileSwitch] = useState(false);
+  const [editThisProfile, setEditThisProfile] = useState(true);
   const dispatch = useDispatch();
 
   async function saveProfileChanges() {
@@ -51,26 +54,39 @@ export default function Profile(props) {
   }
 
   function resetChanges() {
-    setProfileDetails({ name: user.name, aboutMe: user.aboutMe, photoURL: user.photoURL });
+    setProfileDetails({ id: user.googleID, name: user.name, aboutMe: user.aboutMe, photoURL: user.photoURL });
     setProfileEditStatus(false);
+  }
+
+  function selectGroupProfileForEdit() {
+    setProfileSwitch(true);
+    setEditThisProfile(false);
+  }
+
+  function selectUserProfileForEdit() {
+    setProfileDetails({ id: user.googleID, name: user.name, aboutMe: user.aboutMe, photoURL: user.photoURL });
+    setProfileSwitch(false);
+    setEditThisProfile(true);
   }
 
   return (
     <>
       <p className='text-xl font-semibold'>Profiles</p>
       <div>
-        <button className={`py-4 mr-10 border-b-2 border-transparent hover:border-gray-600 hover:text-slate-600 text-sm ${theme.text600} font-semibold`}>User Profile</button>
-        <button className={`py-4 mr-10 border-b-2 border-transparent hover:border-gray-600 hover:text-slate-600 text-sm ${theme.text600} font-semibold`}>Group Profiles</button>
+        <button onClick={() => selectUserProfileForEdit()} className={`py-4 mr-10 border-b-2 border-transparent hover:border-gray-600 hover:text-slate-600 text-sm ${theme.text600} font-semibold`}>User Profile</button>
+        <button onClick={() => selectGroupProfileForEdit()} className={`py-4 mr-10 border-b-2 border-transparent hover:border-gray-600 hover:text-slate-600 text-sm ${theme.text600} font-semibold`}>Group Profiles</button>
       </div>
       <hr className={`mb-3 ${theme.border300}`} />
 
-      <div className='w-full flex'>
+      { profileSwitch ? <SelectionBox profileDetails={profileDetails} setProfileDetails={setProfileDetails} setEditThisProfile={setEditThisProfile} /> : null }
+
+      <div className={`w-full flex ${ editThisProfile ? 'visible' : 'invisible' }`}>
         <div className='w-1/2'>
           <p className='mb-1 font-bold text-xs'>DISPLAY NAME</p>
           <input onChange={(e) => updateProfile(e.target.name, e.target.value)} type="text" name="name" id="" value={profileDetails.name} className={`w-full px-2 py-1 focus:outline-none rounded ${theme.bg100}`} />
           <hr className={`my-3 ${theme.border300}`} />
-          <p className='mb-1 font-bold text-xs'>ABOUT ME</p>
-          <textarea onChange={(e) => updateProfile(e.target.name, e.target.value)} name={'aboutMe'} id="" cols="30" rows="5" value={profileDetails.aboutMe} className={`resize-none focus:outline-none w-full px-2 py-1 rounded ${theme.bg100}`}></textarea>
+          <p className='mb-1 font-bold text-xs'>{ profileSwitch ? 'DESCRIPTION' : 'ABOUT ME'}</p>
+          <textarea onChange={(e) => updateProfile(e.target.name, e.target.value)} name={'aboutMe'} id="" cols="30" rows="5" value={profileSwitch ? profileDetails.description : profileDetails.aboutMe} className={`resize-none focus:outline-none w-full px-2 py-1 rounded ${theme.bg100}`}></textarea>
           <hr className={`my-3 ${theme.border300}`} />
           <p className='mb-1 font-bold text-xs'>PHOTO</p>
           <label className={`px-4 py-1 rounded text-xs ${theme.bg100} ${theme.hoverBg200}`}>Change Photo<input onChange={(e) => updateProfile(e.target.name, e.target.files[0])} type="file" name="photoURL" id="" className='w-0 h-0 hidden' /></label>
@@ -84,13 +100,13 @@ export default function Profile(props) {
             </div>
             <div className={`m-4 mt-14 p-2 text-xs rounded-lg ${theme.bg50}`}>
               <p className='text-lg font-bold'>{ profileDetails.name }</p>
-              <p className='font-semibold'>{ user.googleID }</p>
+              <p className='font-semibold'>{ profileDetails.id }</p>
               <hr className={`my-4 ${theme.border300}`} />
-              <p className='font-semibold'>{ 'ABOUT ME'}</p>
-              <p>{ profileDetails.aboutMe }</p>
+              <p className='font-semibold'>{ profileSwitch ? 'DESCRIPTION' : 'ABOUT ME'}</p>
+              <p>{ profileSwitch ? profileDetails.description : profileDetails.aboutMe }</p>
               <hr className={`my-4 ${theme.border300}`} />
               <p className='font-semibold'>CHATME MEMBER SINCE</p>
-              <p>{ dateFromEpoch(user.doj) }</p>
+              <p>{ dateFromEpoch(profileDetails.doj) }</p>
             </div>
           </div>
         </div>
