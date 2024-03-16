0540925fd33273734e0d64041bb90fdb398614ca
yash0987 <84738197+yash0987@users.noreply.github.com>
2024-03-16 14:37:35 +0530

    Add reducer to update group details

--- client/src/features/auth-slice/authSlice.js
+++ client/src/features/auth-slice/authSlice.js
@@ -11,9 +11,17 @@ export const authSlice = createSlice({
         },
         setUser: (state, action) => {
             state.value.user = action.payload;
+        },
+        setUserGroups: (state, action) => {
+            state.value.user.groups = state.value.user.groups.map((group) => {
+                if (group.id === action.payload.id) {
+                    return action.payload;
+                }
+                return group;
+            })
         }
     }
 })
 
-export const { setAuth, setUser } = authSlice.actions;
+export const { setAuth, setUser, setUserGroups } = authSlice.actions;
 export default authSlice.reducer;
