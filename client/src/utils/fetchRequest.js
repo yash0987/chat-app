d213d1a9ad3d08fa3e33ba93ffb3e87b786559b2
yash0987 <84738197+yash0987@users.noreply.github.com>
2024-03-27 20:32:48 +0530

    Update fetchRequest util function to send body

--- client/src/utils/fetchRequest.js
+++ client/src/utils/fetchRequest.js
@@ -1,13 +1,17 @@
-export async function fetchRequest({ url, method }) {
-    const response = await fetch(url, {
+export async function fetchRequest({ url, method, body }) {
+    const httpHeaders = {
         method,
         credentials: "include",
         headers: {
             Accept: "application/json",
             "Content-Type": "application/json",
             "Access-Control-Allow-Credentials": true
-        }
-    });
+        },
+    };
+    if (method !== 'GET') httpHeaders.body = JSON.stringify(body);
+    console.log(httpHeaders)
+
+    const response = await fetch(url, httpHeaders);
     const data = await response.json();
     return data;
 }
